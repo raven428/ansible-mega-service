4d9ee7c9fecd22e1964cd0f35c22e485c8e4a3db
Dmitry Sukhodoev <raven428@gmail.com>
2026-04-26 14:42:51 +0000

    replace container to appimage

--- molecule/default/run-tests.sh
+++ molecule/default/run-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ueo pipefail
 umask 0022
-MY_BIN="$(readlink -f "$0")"
+MY_BIN="$(realpath "$0")"
 MY_PATH="$(dirname "${MY_BIN}")"
 cd "${MY_PATH}/../.."
 # shellcheck disable=1091
@@ -9,8 +9,28 @@ source "${MY_PATH}/../prepare.sh"
 sce='default'
 LOG_PATH="/tmp/molecule-$(/usr/bin/env date '+%Y%m%d%H%M%S.%3N')"
 printf "\n\n\nmolecule [create] action\n"
-ANSIBLE_LOG_PATH="${LOG_PATH}-00create" \
-  ansible-docker.sh molecule -v create -s ${sce}
+# shellcheck disable=2154
+ANSIBLE_LOG_PATH="${LOG_PATH}-00create" "${_appimage_bin}" molecule -v create -s ${sce}
+# Override galaxy-installed roles with specific commits when requested.
+# Must run after molecule create (which triggers galaxy dependency install).
+if [[ -n "${MEGA_VAR_REF:-}" ]]; then
+  echo "MEGA_VAR_REF=${MEGA_VAR_REF}"
+  /usr/bin/env rm -rf "${ANSIBLE_ROLES_PATH}/raven428.mega_var"
+  /usr/bin/env git clone --branch "${MEGA_VAR_REF}" \
+    'https://github.com/raven428/ansible-mega-var.git' \
+    "${ANSIBLE_ROLES_PATH}/raven428.mega_var"
+else
+  echo 'MEGA_VAR_REF input missed'
+fi
+if [[ -n "${MEGA_LAUNCH_REF:-}" ]]; then
+  echo "MEGA_LAUNCH_REF=${MEGA_LAUNCH_REF}"
+  /usr/bin/env rm -rf "${ANSIBLE_ROLES_PATH}/raven428.mega_launch"
+  /usr/bin/env git clone --branch "${MEGA_LAUNCH_REF}" \
+    'https://github.com/raven428/ansible-mega-launch.git' \
+    "${ANSIBLE_ROLES_PATH}/raven428.mega_launch"
+else
+  echo 'MEGA_LAUNCH_REF input missed'
+fi
 n=1
 run_group() {
   local tag="${1}"
@@ -25,7 +45,7 @@ run_group() {
   printf "\n\n\nmolecule [converge] %s check\n" "${tag:-empty}"
   # shellcheck disable=2086
   ANSIBLE_LOG_PATH="${LOG_PATH}-$(printf %02d $n)converge${prefix}-check" \
-    ansible-docker.sh molecule -v converge -s "${sce}"${args}
+    "${_appimage_bin}" molecule -v converge -s "${sce}"${args}
   ((n++))
   for mode in action check; do
     args=''
@@ -42,7 +62,7 @@ run_group() {
       printf "\n\n\nmolecule [%s] %s %s\n" "${stage}" "${mode}" "${tag}"
       # shellcheck disable=2086
       ANSIBLE_LOG_PATH="${LOG_PATH}-$(printf %02d $n)${stage}${prefix}-${mode}" \
-        ansible-docker.sh molecule -v "${stage}" -s "${sce}"${args}
+        "${_appimage_bin}" molecule -v "${stage}" -s "${sce}"${args}
       ((n++))
     done
   done
@@ -57,7 +77,8 @@ elif [[ "${ANSIBLE_CALL_MODE:-empty}" == 'def2' ]]; then
   printf "\n\n\nmolecule [converge] service-destroy fail\n"
   ANSIBLE_PROP_MODE='fail-destroy' \
     ANSIBLE_LOG_PATH="${LOG_PATH}-$(printf '%02d' "$n")converge-destroy" \
-    ansible-docker.sh molecule -v converge -s "${sce}" -- -t service-gaiad,service-destroy
+    "${_appimage_bin}" molecule -v converge \
+    -s "${sce}" -- -t service-gaiad,service-destroy
   ((n++))
   export ANSIBLE_PROP_MODE='systemd'
   run_group 'service-gaiad,service-stop'
